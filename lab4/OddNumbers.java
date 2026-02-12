5e56a8288080c3f0e8b4c95959850795e0a22a20
Vinuti22 <sh.ok.00@bk.ru>
2026-02-12 16:23:43 +0500

    Выполнил ЛАБ 4

---
+++ lab4/OddNumbers.java
@@ -0,0 +1,12 @@
+public class OddNumbers {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("Нечётные числа от 1 до 100:");
+        do {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+            i++;
+        } while (i <= 100);
+    }
+}
